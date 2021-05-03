4059cebd40d30ad419f7a9bb72d1f169d00a3c28
Feronika Awalia Putri Widjaya <feronikaawalia26@gmail.com>
2021-05-03 10:01:01 +0700

    first commit

---
+++ src/testmultia/TestMultiA.java
@@ -0,0 +1,8 @@
+package testmultia;
+
+public class TestMultiA {
+    public void methodA()
+        throws IOException, RuntimeException{
+            // do some IO stuff
+        }
+}
---
+++ src/testmultia/TestMultiB2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.sql.*;
+
+package testmultia;
+
+public class TestMultiB2 extends TestMultiA {
+    public void methodA()
+            throws FileNotFoundException, UTFDataFormatException, 
+                ArithmeticException, SQLException{
+        // do some IO, number crunching, and SQL stuff
+    }
+} 
+
